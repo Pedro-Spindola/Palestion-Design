31aba8ab7acb1027a051c893bec7b7e334f07612
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2023-04-17 23:34:39 -0300

    Update

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+});
+
+$(function() {
+    $('.mod_single1').click(function() {
+      $(this).next('.mod_single2').slideToggle();
+      $(this).toggleClass("aberto");
+      $('.mod_single2').not($(this).next('.mod_single2')).slideUp();
+      $('.mod_single1').not($(this)).removeClass("aberto");
+    });
+  });
+  $(function(){
+    $('.titulo').hover(function(){
+      $(this).next('.temas').css('display', 'block');
+      $('.temas').not($(this).next('.temas')).css('display', 'none');
+    });
+    $('.titulo').mouseleave(function(){
+      $('.temas').css('display', 'none');
+    });
+  });
+  
